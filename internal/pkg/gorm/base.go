07a3b3817d29ecb520ef1e7b2a23111f27e99d13
lasndnansdl <flihjkcey@gmail.com>
2025-04-28 14:30:34 +0500

    fix swagger types

--- internal/pkg/gorm/base.go
+++ internal/pkg/gorm/base.go
@@ -2,7 +2,6 @@ package gorm
 
 import (
 	"time"
-	"gorm.io/gorm"
 )
 
 // Base содержит общие поля для всех таблиц
@@ -10,17 +9,13 @@ import (
 type Base struct {
 	// @Description Уникальный идентификатор
 	// @example 1
-	ID        uint           `gorm:"primarykey" json:"id"`
+	ID        uint      `gorm:"primarykey" json:"id"`
 	
 	// @Description Время создания записи
 	// @example 2025-04-28T12:00:00Z
-	CreatedAt time.Time      `json:"created_at"`
+	CreatedAt time.Time `json:"created_at"`
 	
 	// @Description Время последнего обновления записи
 	// @example 2025-04-28T12:00:00Z
-	UpdatedAt time.Time      `json:"updated_at"`
-	
-	// @Description Время удаления записи (для мягкого удаления)
-	// @example 2025-04-28T12:00:00Z
-	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at"`
 } 
